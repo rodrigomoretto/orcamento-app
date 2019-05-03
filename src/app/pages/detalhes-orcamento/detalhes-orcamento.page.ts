b3c4e18fe19fb5426562abc9ad9ec8d65fe06a61
Rodrigo Moretto <rodrigo-moretto@hotmail.com>
2019-05-03 09:41:13 -0300

    Ajustes Detalhes Orçamentos

--- src/app/pages/detalhes-orcamento/detalhes-orcamento.page.ts
+++ src/app/pages/detalhes-orcamento/detalhes-orcamento.page.ts
@@ -1,49 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Produto } from 'src/app/models/produto';
-import { OrcamentoService } from 'src/app/services/orcamento.service';
+import { Component } from '@angular/core';
 import { Orcamento } from 'src/app/models/orcamento';
-import { HttpErrorResponse } from '@angular/common/http';
-import { NavParams } from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Cliente } from 'src/app/models/cliente';
-import { Vendedor } from 'src/app/models/vendedor';
 
 @Component({
   selector: 'app-detalhes-orcamento',
   templateUrl: './detalhes-orcamento.page.html',
   styleUrls: ['./detalhes-orcamento.page.scss'],
 })
-export class DetalhesOrcamentoPage implements OnInit {
+export class DetalhesOrcamentoPage {
 
   public orcamento: Orcamento;
-  public produtos: Produto[];
-  public itens: Array<any>;
-  public cliente: Cliente;
-  public vendedor: Vendedor;
 
   constructor(private _activatedRoute: ActivatedRoute, private _router: Router) {
 
-    this._activatedRoute.queryParams.subscribe(params => {
-      if (this._router.getCurrentNavigation().extras.state) {
-        this.orcamento = this._router.getCurrentNavigation().extras.state.orcamento;
-        console.log(this.orcamento);
-      }
-    });
-    console.log(this.orcamento);
-
-    // this.carro = this.navParams.get('carroSelecionado');
-    // this.orcamentos = this.navParams.get('orcamento');
-    // console.log(this.orcamentos)
-  }
-
-  ngOnInit() {
-    // this._activatedRoute.params.subscribe(params => {
-    //   this.orcamentos = params['id'];
-    //   console.log(this.orcamentos);
-    // });
-
-    // console.log(this._activatedRoute.snapshot.data);
-    // console.log(this.orcamentos_id);
+    this._activatedRoute.queryParams
+      .subscribe(params => {
+        if (this._router.getCurrentNavigation().extras.state) {
+          this.orcamento = this._router.getCurrentNavigation().extras.state.orcamento;
+          // console.log(this.orcamento);
+        }
+      });
   }
 
 }
--- src/app/services/object-access.service.ts
+++ src/app/services/object-access.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ObjectAccessService {
 
-  constructor() { }
+  object: any;
+
+  constructor(private _activatedRoute: ActivatedRoute, private _router: Router) {
+
+  }
+
+  getObject(activedRoute, router): void {
+    activedRoute.queryParams.subscribe(params => {
+      if (router.getCurrentNavigation().extras.state) {
+        this.object = router.getCurrentNavigation().extras.state;
+      }
+    });
+
+
+      console.log(this.object);
+
+
+    // return this._activatedRoute.queryParams
+    //   .subscribe(params => {
+    //     if (this._router.getCurrentNavigation().extras.state) {
+    //       this.object = this._router.getCurrentNavigation().extras.state[nomeObject]
+    //       console.log(this.object);
+    //       return this.object;
+    //     }
+    //   });
+  }
 }
